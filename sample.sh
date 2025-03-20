3d12564702295f02c10b3a17afdb7381115d426e
Junliang Ye <145889120+JAMESYJL@users.noreply.github.com>
2025-03-20 10:22:42 +0800

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc-per-node=0 --master_port=12345 sample.py \
+    --model_path "your_model_path" \
+    --steps 120000 \
+    --input_path example \
+    --output_path mesh_output \
+    --repeat_num 4 \
+    --uid_list "" \
+    --temperature 0.5 \
