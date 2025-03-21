10902033a44fedb08c0fd7e9e563b5034f4dda41
Junliang Ye <145889120+JAMESYJL@users.noreply.github.com>
2025-03-21 13:30:43 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 torchrun --nproc-per-node=0 --master_port=12345 sample.py \
     --model_path "your_model_path" \
     --steps 120000 \
-    --input_path example \
+    --input_path examples \
     --output_path mesh_output \
     --repeat_num 4 \
     --uid_list "" \
